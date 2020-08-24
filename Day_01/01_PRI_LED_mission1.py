4458ddc4495f297199740f766686774eb170de0c
gkrry2723 <51503799+gkrry2723@users.noreply.github.com>
2020-08-24 12:08:38 +0900

    Update 01_PRI_LED_mission1.py
    주석 추가

--- Day_01/01_PRI_LED_mission1.py
+++ Day_01/01_PRI_LED_mission1.py
@@ -1,6 +1,12 @@
 import RPi.GPIO as GPIO
 import time
 
+# 20184754 김현주, 20184487 유채림 ---- day 1. mission1 -----
+# mission: 센서등 시스템 설계
+# 내용:  1. 시스템이 작동가능한 상태가 되면 Detect Ready 문구를 출력한다.
+#       2. 센서에 움직임이 detect되면 Detect 문구가 출력되고 문이 LED에 불이 들어온다.
+#       3. 센서에 움직임이 detect되지 않으면 Where are U 문구가 출력되고 LED에 불이 들어오지 않는다.
+#       4. KeyboardInterrupt가 발생하면 작동을 멈춘다.
 
 sensor = 4
 led_pin1 = 8
@@ -10,7 +16,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(sensor, GPIO.IN)
 GPIO.setup(led_pin1, GPIO.OUT)
 
-print("Delect READY___!@!!!!!")
+print("Detect READY___!@!!!!!")
 time.sleep(3)   #3sec waiting
 
 
